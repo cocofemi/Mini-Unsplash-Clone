6cafef4634804f302c165c0a89e4a36466709a00
cocofemi@gmail.com <cocofemi@gmail.com>
2021-02-17 10:48:54 +0100

    wsgi file change

--- backend/src/mini_unsplash_clone/settings.py
+++ backend/src/mini_unsplash_clone/settings.py
@@ -64,6 +64,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+
 ]
 
 ROOT_URLCONF = 'mini_unsplash_clone.urls'
--- backend/src/mini_unsplash_clone/wsgi.py
+++ backend/src/mini_unsplash_clone/wsgi.py
@@ -10,13 +10,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import DjangoWhiteNoise
 from dotenv import load_dotenv
 
-project_folder = os.path.expanduser('C:\Users\HP\Desktop\djreact')
+project_folder = os.path.expanduser('~/djreact')
 load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mini_unsplash_clone.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
