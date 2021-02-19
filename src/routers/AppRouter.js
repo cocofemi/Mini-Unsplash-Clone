5658e88aa006d5c2764b43481e3e73150c9c3156
cocofemi@gmail.com <cocofemi@gmail.com>
2021-02-19 12:31:31 +0100

    new files

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginPage from '../components/LoginPage';
 import CustomLayout from '../components/CustomLayout';
 import NotFoundPage from '../components/NotFoundPage';
