dae9ff52208591b641e7442ecd3d166ac1bc96c9
Ogundayo Femi <cocofemi@gmail.com>
2022-10-11 15:41:36 +0100

    Update settings.py

--- mini_unsplash_clone/settings.py
+++ mini_unsplash_clone/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
+    'mini_unsplash_clone'
     'rest_framework',
     'corsheaders',
     'posts',
